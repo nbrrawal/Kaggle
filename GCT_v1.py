c891a5a41b427dd1da7a48142d5cf68bc1df74c9
nbrrawal <nbrrawal@rediffmail.com>
2018-07-21 21:16:46 +0530

    completing the code so far

--- GCT_v1.py
+++ GCT_v1.py
@@ -42,5 +42,59 @@ dt.describe(include="all")
 print('Country or Area')
 dt['country_or_area'].nunique()
 dt['country_or_area'].unique()
-#there are 209 overall country or areas present in the data, Now lets see frequencies of each of these catergories.. only top 15, since there are a lot of categories
+#there are 209 overall country or areas present in the data,
+#Now lets see frequencies of each of these catergories.. only top 15, since there are a lot of categories
 dt['country_or_area'].value_counts()[:15]
+
+print('Commodity')
+dt['commodity'].nunique()
+dt['commodity'].unique()
+dt['commodity'].value_counts()[:20]
+dt.commodity.unique()
+
+print('flow variable')
+dt['flow'].nunique()
+dt['flow'].unique()
+dt['flow'].value_counts()
+print('category')
+dt['category'].nunique()
+dt['category'].unique()
+#there are 209 overall country or areas present in the data, Now lets see frequencies of each of these catergories, pick first 20 most occured
+dt['category'].value_counts()[:20]
+
+dfSheeps = dt[dt['commodity']=='Sheep, live'].reset_index(drop=True)
+dfGoats = dt[dt['commodity']=='Goats, live'].reset_index(drop=True)
+dfSheeps.head()
+dfSheepsGrouped = pd.DataFrame({'weight_kg' : dfSheeps.groupby( ["year","flow","commodity"] )["weight_kg"].sum()}).reset_index()
+dfGoatsGrouped = pd.DataFrame({'weight_kg' : dfGoats.groupby( ["year","flow","commodity"] )["weight_kg"].sum()}).reset_index()
+dfSheepsGrouped.head()
+
+f, ax = plt.subplots(1, 1)
+dfgr = pd.concat([dfSheepsGrouped,dfGoatsGrouped])
+ax = sns.pointplot(ax=ax,x="year",y="weight_kg",data=dfgr[dfgr['flow']=='Import'],hue='commodity')
+_ = ax.set_title('Global imports of kgs by animal')
+
+
+
+dfSheepsGrouped = pd.DataFrame({'weight_kg' : dfSheeps.groupby( ["country_or_area","flow","commodity"] )["weight_kg"].sum()}).reset_index()
+dfSheepsGrouped.head()
+
+sheepsImportsCountry = dfSheepsGrouped[dfSheepsGrouped['flow']=='Import']
+sheepsExportsCountry = dfSheepsGrouped[dfSheepsGrouped['flow']=='Export']
+sheepsImportsCountry.head()
+
+ax = sns.barplot(x="weight_kg", y="country_or_area", data=sheepsImportsCountry.sort_values('weight_kg',ascending=False)[:15])
+_ = ax.set(xlabel='Kgs', ylabel='Country or area',title = "Countries or areas that imported more kgs of Sheeps")
+
+ax = sns.barplot(x="weight_kg", y="country_or_area", data=sheepsExportsCountry.sort_values('weight_kg',ascending=False)[:15])
+_ = ax.set(xlabel='Kgs', ylabel='Country or area',title = "Countries or areas that exported more kgs of Sheeps")
+
+#List of top 10 countries having highest trade in terms of dollars
+dt.groupby(['country_or_area'])['trade_usd'].aggregate(np.sum).nlargest(10)
+
+#List of top 10 countries having smallest trade in terms of dollars
+dt.groupby(['country_or_area'])['trade_usd'].aggregate(np.sum).nsmallest(10)
+#List of top 10 countries having highest trade in terms of weight
+dt.groupby(['country_or_area'])['weight_kg'].aggregate(np.sum).nlargest(10)
+#List of top 10 countries having smallest trade in terms of weight
+dt.groupby(['country_or_area'])['weight_kg'].aggregate(np.sum).nsmallest(10)
